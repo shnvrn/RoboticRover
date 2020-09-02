33b12ba5ca14e919ee58f6b66bee2912481b289a
shnvrn <66789922+shnvrn@users.noreply.github.com>
2020-09-02 19:15:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboticRovers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RoboticRover roverManager = new RoboticRover();
+            string location = string.Empty;
+
+            List<string> roverCurrentLocation = new List<string>();
+            roverCurrentLocation.Add("1,2,N");
+            roverCurrentLocation.Add("3,3,E");
+
+            List<string> roverMovesList = new List<string>();
+            roverMovesList.Add("LMLMLMLMM");
+            roverMovesList.Add("MMRMMRMRRM");
+
+            for (int i = 0; i < roverMovesList.Count; i++)
+            {
+                Console.WriteLine(string.Format("{0},{1},{2}","Rover started to move", "Commands" + "=" + roverMovesList[i], "Current Location" + "=" + roverCurrentLocation[i]));
+                location = roverManager.Move(roverMovesList[i], roverCurrentLocation[i].Replace(",", "").Trim());
+                Console.WriteLine(location);
+
+                if (i != roverMovesList.Count - 1)
+                    Console.WriteLine("----------------------------------------------------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RoboticRover.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboticRovers
+{
+    public class RoboticRover
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+        private string direction { get; set; }
+
+        public void GoForward()
+        {
+            switch (direction)
+            {
+                case "N":
+                    y += 1;
+                    break;
+                case "W":
+                    x -= 1;
+                    break;
+                case "S":
+                    y -= 1;
+                    break;
+                case "E":
+                    x += 1;
+                    break;
+            }
+
+        }
+        public void TurnLeft()
+        {
+            switch (direction)
+            {
+                case "N":
+                    direction = "W";
+                    break;
+                case "W":
+                    direction = "S";
+                    break;
+                case "S":
+                    direction = "E";
+                    break;
+                case "E":
+                    direction = "N";
+                    break;
+            }
+
+        }
+        public void TurnRight()
+        {
+            switch (direction)
+            {
+                case "N":
+                    direction = "E";
+                    break;
+                case "E":
+                    direction = "S";
+                    break;
+                case "S":
+                    direction = "W";
+                    break;
+                case "W":
+                    direction = "N";
+                    break;
+            }
+
+        }
+        public string Move(string roverMove, string roverCurrentLocation)
+        {
+            char[] arrRoverMove = roverMove.ToCharArray();
+            char[] arrRoverCurrentLocation = roverCurrentLocation.ToCharArray();
+
+            string currentLocation = roverCurrentLocation;
+            x = Convert.ToInt32(arrRoverCurrentLocation[0].ToString());
+            y = Convert.ToInt32(arrRoverCurrentLocation[1].ToString());
+            direction = arrRoverCurrentLocation[2].ToString();
+
+            for (int i = 0; i < arrRoverMove.Length; i++)
+            {
+                switch (arrRoverMove[i])
+                {
+                    case 'L':
+                        TurnLeft();
+                        break;
+                    case 'R':
+                        TurnRight();
+                        break;
+                    case 'M':
+                        GoForward();
+                        break;
+                }
+                Console.WriteLine((i + 1) + "." + "Move :" + x + " " + y + " " + direction + " " + "Command:" + arrRoverMove[i]);
+            }
+
+            return ("Last Position:" + x + " " + y + " " + direction);
+        }
+
+    }
+}
